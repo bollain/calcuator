95a977da9cd62fc3deb31419663986786bf89ef7
bollain <bollain@gmail.com>
2017-06-21 21:09:40 -0700

    First commit on calculator

---
+++ CalculatorBrain.swift
@@ -0,0 +1,92 @@
+//
+//  CalculatorBrain.swift
+//  calculator
+//
+//  Created by Armando Dorantes Bollain y Goytia on 2017-06-19.
+//  Copyright © 2017 Armando Dorantes Bollain y Goytia. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    
+    private enum Operation{
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double, Double) -> Double)
+        case equals
+        case clear
+    }
+    
+    private var operations: Dictionary<String, Operation> =
+        [
+            "π": Operation.constant(Double.pi),
+            "e": Operation.constant(M_E),
+            "√": Operation.unaryOperation(sqrt),
+            "cos": Operation.unaryOperation(cos),
+            "sin": Operation.unaryOperation(sin),
+            "٪": Operation.unaryOperation({$0 / 100}),
+            "±": Operation.unaryOperation({-$0}),
+            "×": Operation.binaryOperation({$0 * $1}),
+            "÷": Operation.binaryOperation({$0 / $1}),
+            "+": Operation.binaryOperation({$0 + $1}),
+            "−": Operation.binaryOperation({$0 - $1}),
+            "=": Operation.equals,
+            "AC": Operation.clear
+    ]
+    
+    mutating func performOperation(_ symbol: String){
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case .equals:
+                performPendingBinaryOperation()
+            case .clear:
+                pendingBinaryOperation = nil
+                accumulator = 0
+            }
+        }
+        
+    }
+    
+    private mutating func performPendingBinaryOperation(){
+        if pendingBinaryOperation != nil && accumulator != nil{
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+        
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation{
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand:Double) -> Double{
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double){
+        accumulator = operand
+    }
+    
+    var result: Double? {
+        get{
+            return accumulator
+        }
+    }
+}
---
+++ calculator/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Armando Dorantes Bollain y Goytia on 2017-06-14.
+//  Copyright © 2017 Armando Dorantes Bollain y Goytia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var display: UILabel!
+    var userIsInMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            if digit != "." || !textCurrentlyInDisplay.contains("."){
+                display.text = textCurrentlyInDisplay + digit
+            }
+            
+        } else {
+            display.text = digit == "." ? "0." : digit
+            userIsInMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInMiddleOfTyping = false
+        }
+        
+        if let mathematicalOperation = sender.currentTitle {
+            brain.performOperation(mathematicalOperation)
+        }
+        if let result = brain.result{
+            displayValue = result
+        }
+    }
+    
+}
+
